1ff4abe09e1195e2cbb80d7728898d4834067731
Melissafl <melissa.fulopz@gmail.com>
2023-04-15 09:55:48 -0600

    repaso async

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
-console.log("Hola");
+const promesa1 = new Promise((resolve, reject) => {
+  const usuarios = [
+    {
+      id: 1,
+      nombre: "Jesus",
+      activo: false,
+    },
+    {
+      id: 2,
+      nombre: "Melissa",
+      activo: true,
+    },
+  ];
+
+  if (usuarios.length > 0) {
+    const resp = {
+      usuarios,
+      msg: "Usuarios obtenidos",
+    };
+
+    resolve(resp);
+  } else {
+    const resp = {
+      usuarios,
+      msgh: "No existen usuarios",
+    };
+    reject(resp);
+  }
+});
+
+const llamarApisPromise = () => {
+  promesa1
+    .then((respuesta) => {
+      console.log(respuesta);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const llamarApisAsyncAwait = async () => {
+  try {
+    const respuesta = await promesa1;
+    console.log(respuesta);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//llamarApisPromise();
+llamarApisAsyncAwait();
