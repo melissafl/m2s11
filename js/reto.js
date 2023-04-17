22745bb72d499ee8243599c9efedfc2f8253f1bf
Melissafl <melissa.fulopz@gmail.com>
2023-04-16 19:36:59 -0600

    retos

---
+++ js/reto.js
@@ -0,0 +1,4 @@
+const nombre = document.getElementById("firstname");
+const apellido = document.getElementById("lastname");
+const nombre = document.getElementById("email");
+const nombre = document.getElementById("pasword");
