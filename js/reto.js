8865ac7493b62d34686815526f65795f9ae47652
Melissafl <melissa.fulopz@gmail.com>
2023-04-16 20:13:45 -0600

    reto11

--- js/reto.js
+++
@@ -1,4 +0,0 @@
-const nombre = document.getElementById("firstname");
-const apellido = document.getElementById("lastname");
-const nombre = document.getElementById("email");
-const nombre = document.getElementById("pasword");
